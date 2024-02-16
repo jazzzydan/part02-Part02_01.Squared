f7cf9ac8f82384790fef2718704adcc719c379d1
Daniil Starov <jazzzydan@hotmail.com>
2024-02-16 17:23:33 +0200

    Initial commit

---
+++ src/main/java/Squared.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Squared {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.valueOf(scanner.nextLine());
+        int squared = number * number;
+        System.out.println(squared);
+    }
+}
